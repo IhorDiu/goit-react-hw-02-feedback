af0a225f6ad4adb03a75acf134bd06b0c4c7f2ae
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-03-12 16:42:23 +0200

    Task_2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,37 @@
-import { FeedbackWidget } from './FeedbackWidget/FeedbackWidget';
+// import { FeedbackWidget } from './FeedbackWidget/FeedbackWidget';
+import { Component } from 'react';
+import { Section } from './Section/Section';
 
-export const App = () => {
-  return (
-    <>
-      <h1>Feedback Form</h1>
-      <FeedbackWidget />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {};
+
+  countPositiveFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <div className="FeedbackWidget">
+        <h1>Feedback Form</h1>
+
+        <Section title="Please leave your feedback">
+          <button type="button">Good</button>
+          <button type="button">Neutral</button>
+          <button type="button">Bad</button>
+        </Section>
+
+        <Section title="Statistics">
+          <span>Good: {}</span>
+          <span>Neutral: {}</span>
+          <span>Bad: {}</span>
+          <span>Total: {}</span>
+          <span>Positive feedback: {}</span>
+        </Section>
+      </div>
+    );
+  }
+}
