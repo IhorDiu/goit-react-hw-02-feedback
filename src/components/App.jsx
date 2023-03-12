fd029f6a2f155531445ee589d86d4cc0e9466394
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-03-12 19:14:38 +0200

    Step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 // import { FeedbackWidget } from './FeedbackWidget/FeedbackWidget';
 import { Component } from 'react';
 import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+
+
 
 export class App extends Component {
   state = {
@@ -9,29 +13,45 @@ export class App extends Component {
     bad: 0,
   };
 
-  countTotalFeedback = () => {};
+  addFeedback = e => {
+    this.setState(prevState => ({ [e]: prevState[e] + 1 }));
+  };
 
-  countPositiveFeedbackPercentage = () => {};
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return good ? Math.round((good / total) * 100) : 0;
+  };
 
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <div className="FeedbackWidget">
         <h1>Feedback Form</h1>
 
         <Section title="Please leave your feedback">
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.addFeedback}
+          />
         </Section>
 
         <Section title="Statistics">
-          <span>Good: {}</span>
-          <span>Neutral: {}</span>
-          <span>Bad: {}</span>
-          <span>Total: {}</span>
-          <span>Positive feedback: {}</span>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
         </Section>
       </div>
     );
   }
 }
+
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,3 +1,23 @@
-export const FeedbackOptions = {
-    
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+          key={option}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
 }
+
